c901f4f6ce0193e3d43123418a62a611fd992f1e
Alxlx97 <Alexandre1997Lemieux@outlook.com>
2026-03-04 16:21:24 -0500

    Implementation of the beer model

---
+++ Models/Beer.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+
+namespace BeerTracker.Models;
+
+public enum ServingSize
+{
+    Ml355 = 355,
+    Ml500 = 500,
+    Ml750 = 750,
+    Ml1000 = 1000
+}
+
+public class Beer
+{
+    private int _id;
+    
+    private string _name;
+    
+    private int _quantity;
+
+    private ServingSize _servingSize;
+
+    public int Id => _id;
+    
+    public string Name { get => _name; set => _name = value; }
+    
+    public int Quantity { get => _quantity; set => _quantity = value; }
+    
+    public ServingSize ServingSize { get => _servingSize; set => _servingSize = value; }
+
+    public Beer(int id, string name, int quantity, ServingSize servingSize)
+    {
+        _id = id;
+        _name = name;
+        _quantity = quantity;
+        _servingSize = servingSize;
+    }
+    
+}
